83d3ee8806c3a30d0cfb12c18fb4fbb4923072d8
Valentin Saraiva <valentinsa0701@gmail.com>
2026-01-09 16:13:17 +0100

    Finalisation TP résilience avec Redis et Postgres

--- api/main.py
+++ api/main.py
@@ -1,11 +1,11 @@
 import psycopg2
 import os
+import redis
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -14,7 +14,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-conn = psycopg2.connect(
+# Connexion Redis
+r = redis.Redis(host=os.getenv("REDIS_HOST", "redis"), port=6379, decode_responses=True)
+
+def get_db_connection():
+    return psycopg2.connect(
         host=os.getenv("POSTGRES_HOST"),
         port=5432,
         user=os.getenv("POSTGRES_USER", "postgres"),
@@ -22,22 +26,35 @@ conn = psycopg2.connect(
         database=os.getenv("POSTGRES_DB", "mydb")
     )
 
-@app.get("/users")
-async def get_users():
-    cursor = conn.cursor()
-    sql_select_query = "SELECT * FROM users"
-    cursor.execute(sql_select_query)
-    records = cursor.fetchall()
-    print("Nombre d'utilisateurs trouvés :", cursor.rowcount)
-    cursor.close()
-    return { "utilisateurs": records }
+@app.get("/")
+async def get_dashboard_data():
+    results = []
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute("SELECT id, nom, prenom, email, promo FROM students")
+        rows = cursor.fetchall()
+        colnames = [desc[0] for desc in cursor.description]
+        
+        for row in rows:
+            student = dict(zip(colnames, row))    
+            student_id = student['id']
+            redis_key = f"student:{student_id}:views"
+            
+            try:
+                views = r.incr(redis_key)
+                student['views'] = views
+            except redis.ConnectionError:
+                student['views'] = "N/A (Cache HS)"
 
-@app.get("/students")
-async def get_students():
-    cursor = conn.cursor()
-    sql_select_query = "SELECT * FROM students"
-    cursor.execute(sql_select_query)
-    records = cursor.fetchall()
-    print("Nombre d'étudiants trouvés :", cursor.rowcount)
-    cursor.close()
-    return { "étudiants": records }
+            results.append(student)
+
+        cursor.close()
+        conn.close()
+        
+    except Exception as e:
+        print(f"Erreur API: {e}")
+        return [{"nom": "Erreur", "promo": str(e)}]
+
+    return results
